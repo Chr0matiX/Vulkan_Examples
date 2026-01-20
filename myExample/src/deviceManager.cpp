95b991df334a599ea1c6e04d03f8f18a3aa9e980
PMComp <18872123146@163.com>
2026-01-20 17:25:11 +0800

    deviceManager 实现

--- myExample/src/deviceManager.cpp
+++ myExample/src/deviceManager.cpp
@@ -1,6 +1,9 @@
 #include "deviceManager.h"
+#include "vulkan/vulkan_core.h"
+#include "vulkanManager.h"
 
 #include <cassert>
+#include <cstdint>
 #include <iostream>
 
 CDeviceManager * CDeviceManager::m_DeviceManagerInstance{nullptr};
@@ -18,8 +21,22 @@ bool CDeviceManager::initManager() {
 	bool rtn = false;
 
 	do {
+		uint32_t gpuCount{0};
+		vkEnumeratePhysicalDevices(CVulkanManager::getInstance().getVkInstance(), &gpuCount,
+								   nullptr);
+		if (gpuCount <= 0) {
+			std::cerr << "gpuCount is zero!\n";
+			break;
+		}
 
-		
+		std::vector<VkPhysicalDevice> physicalDevices(gpuCount);
+		if (vkEnumeratePhysicalDevices(CVulkanManager::getInstance().getVkInstance(), &gpuCount,
+									   nullptr) != VK_SUCCESS) {
+			std::cerr << "vkEnumeratePhysicalDevices failed!\n";
+			break;
+		}
+
+		// gpu 打分选取
 
 		if (!valid())
 			break;
